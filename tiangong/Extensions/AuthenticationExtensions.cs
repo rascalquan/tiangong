22c794210614409aed0186870574e4d97c5b7cab
YangXiaoQuan <yangxiaoquan4402@dingtalk.com>
2020-09-21 09:04:45 +0800

    move jwt&swagger to extensions

---
+++ tiangong/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tiangong.Config;
+
+namespace tiangong.Extensions
+{
+    public static class AuthenticationExtensions
+    {
+        public static IServiceCollection AddAuthenticationConfig(this IServiceCollection services,IConfiguration Configuration)
+        {
+            //配置jwt验证
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                var jwtConfig = Configuration.GetSection(JWTConfig.SectionName).Get<JWTConfig>();
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,//是否验证Issuer
+                    ValidateAudience = true,//是否验证Audience
+                    ValidateLifetime = true,//是否验证失效时间
+                    ClockSkew = TimeSpan.FromSeconds(30),//时钟偏差(秒)
+                    ValidateIssuerSigningKey = true,//是否验证SecurityKey
+                    ValidAudience = jwtConfig.Audience,//Audience
+                    ValidIssuer = jwtConfig.Issuer,//Issuer，与签发jwt的设置一致
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig.SecurityKey))//拿到SecurityKey
+                };
+
+            });
+
+            return services;
+        }
+    }
+}
---
+++ tiangong/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tiangong.Extensions
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            //配置swagger
+            services.AddSwaggerGen(options =>
+            {
+                //配置swagger文档基本信息
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "天宫",
+                    Version = "v1.0.0",
+                    Description = "天宫接口文档",
+                    Contact = new OpenApiContact() { Name = "rascal" }
+                });
+
+                //配置接口注释
+                //并加入控制器注释
+                options.IncludeXmlComments(
+                    filePath: Path.Combine(AppContext.BaseDirectory, "tiangong.xml"),
+                    includeControllerXmlComments: true
+                    );
+
+                //配置授权：
+                //在header中添加token头
+                options.OperationFilter<AddResponseHeadersFilter>();
+                options.OperationFilter<AppendAuthorizeToSummaryOperationFilter>();
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+
+                //配置jwt
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme()
+                {
+                    Description = "JWT授权（数据将在header中），直接在下框中输入 Bearer {token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+            });
+
+            return services;
+        }
+    }
+}
